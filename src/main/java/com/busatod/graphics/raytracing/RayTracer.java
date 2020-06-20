75bc39d0034f81a205b4ab43154862bc2cd833ff
dbusato <davidedbusato@gmail.com>
2020-06-20 17:05:26 +0200

    Fixing gradle

--- src/main/java/com/busatod/graphics/raytracing/RayTracer.java
+++ src/main/java/com/busatod/graphics/raytracing/RayTracer.java
@@ -1,8 +1,39 @@
 package com.busatod.graphics.raytracing;
 
-public class RayTracer {
+import main.java.com.busatod.graphics.app.GraphicsApplication;
+import main.java.com.busatod.graphics.app.Settings;
+
+public class RayTracer extends GraphicsApplication {
+
 	public static void main(String[] args) {
-		System.out.println("Ray Tracing!!");
+		//		System.out.println("Ray Tracing!!");
+		new RayTracer();
+	}
+
+	public RayTracer() {
+		Settings settings = new Settings();
+		settings.title = "Ray Tracing!!";
+		start(settings);
+	}
+
+	@Override
+	protected void appInit() {
+
+	}
+
+	@Override
+	protected void appUpdate(long elapsedTime) {
+
+	}
+
+	@Override
+	protected void appFinishOff() {
+
+	}
+
+	@Override
+	protected void appPrintFinalStats() {
+
 	}
 }
 
