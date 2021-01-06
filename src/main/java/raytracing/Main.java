98922e8990f2ae566c1722683a99d4ae2f25f614
dbusato <davidedbusato@gmail.com>
2021-01-06 15:10:12 +0100

    Fix

--- src/main/java/raytracing/Main.java
+++ src/main/java/raytracing/Main.java
@@ -3,34 +3,34 @@ package raytracing;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-import base.graphics.app.GraphicsAppSoftRendering;
+import base.graphics.app.GraphAppSoftRendering;
 import base.graphics.app.Settings;
 import raytracing.worlds.SingleSphere;
 import raytracing.worlds.World;
 
-public class Main extends GraphicsAppSoftRendering {
+public class Main extends GraphAppSoftRendering {
 	Settings settings;
 	private World world;
 
-	public Main() {
+	public Main() throws Exception {
 		settings = new Settings();
-		settings.width = 300;
-		settings.height = 300;
+		settings.winWidth = 300;
+		settings.winHeight = 300;
 		settings.title = "Ray tracing";
 		settings.showMenu = true;
 		start(settings);
 	}
 
+	public static void main(String[] args) throws Exception {
+		new Main();
+	}
+
 	@Override
-	protected void appInit() {
+	public void initApp() {
 		initFrameImage();
 //		world = new MultipleObjects(this);
 		world = new SingleSphere(this);
 		world.build(frameImage.getWidth(), frameImage.getHeight());
-	}
-
-	@Override
-	protected void appUpdate(long elapsedTime) {
 	}
 
 	@Override
@@ -40,20 +40,27 @@ public class Main extends GraphicsAppSoftRendering {
 		world.renderScene();
 	}
 
-	@Override
-	protected void appFinishOff() {
-	}
-
-	@Override
-	protected void appPrintFinalStats() {
-	}
-
 	public void setPixel(int x, int y, int r, int g, int b) {
 		int pos = frameImage.getWidth() * y + x; // *4 implicit
 		pixels[pos] = (0xFF << 24) | (r << 16) | (g << 8) | b;
 	}
 
-	public static void main(String[] args) {
-		new Main();
+	@Override
+	public void updateApp(long elapsedTime) {
+		// TODO Auto-generated method stub
+
 	}
+
+	@Override
+	public void finishOffApp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printFinalStatsApp() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
